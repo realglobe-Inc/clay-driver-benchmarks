8657747e08ebd4f8669e9580fd3b07550b046b75
Taka Okunishi <okunishinish@gmail.com>
2017-03-17 10:39:43 +0900

    driverのbenchmarkを図る仕組みを用意した

---
+++ test/benchmark_drop_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for benchmarkDrop.
+ * Runs with mocha.
+ */
+'use strict'
+
+const benchmarkDrop = require('../lib/cases/benchmark_drop.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('benchmark-drop', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Benchmark drop', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
---
+++ test/benchmark_list_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for benchmarkList.
+ * Runs with mocha.
+ */
+'use strict'
+
+const benchmarkList = require('../lib/cases/benchmark_list.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('benchmark-list', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Benchmark list', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
